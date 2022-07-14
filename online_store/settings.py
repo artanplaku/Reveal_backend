43a7374574e69a1cdca6fbbf901091d055b9aadc
Artan <artanpl03@gmail.com>
2022-07-14 14:09:14 -0400

    changes to secretkey

--- online_store/settings.py
+++ online_store/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bppw&^g_b04ouzhoh4&i(t*vus9(d%ahw*7xy6!vrl2$8zez=q'
+# SECRET_KEY = 'django-insecure-bppw&^g_b04ouzhoh4&i(t*vus9(d%ahw*7xy6!vrl2$8zez=q'
+SECRET_KEY = config('SECRET_KEY')DEBUG = config('DEBUG', default=False, cast=bool)DATABASES = { 'default': dj_database_url.config( default=config('DATABASE_URL') )}
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
